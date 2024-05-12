b349e006c30de697b14affbc00552498e8eed598
Chris Cashman <chris.cashman91@gmail.com>
2024-05-13 00:43:43 +0100

    Added directory walker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, Arg};
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("WebP to PNG Converter")
+        .version("1.0")
+        .author("Chris Cashman")
+        .about("Converts all WebP images in a directory to PNG format")
+        .arg(
+            Arg::with_name("directory")
+                .short('d')
+                .long("directory")
+                .takes_value(true)
+                .required(true)
+                .help("The directory to search for WebP files"),
+        )
+        .get_matches();
+
+    let directory = matches.value_of("directory").unwrap();
+
+    for entry in WalkDir::new(directory) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        println!("{:?}", path);
+    }
 }
